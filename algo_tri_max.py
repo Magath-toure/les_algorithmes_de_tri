e97b63ddd3b2fc9bee70889d233d42fef42b35d4
Sengsathit <sengsathit@gmail.com>
2023-12-22 14:58:06 +0100

    second tri

--- algo_tri_max.py
+++ algo_tri_max.py
@@ -1,14 +1,39 @@
+import time
+
 # première méthode de tri, avec la boucle while
-a_trier = [14, 12, 16, 8, 9, 10, 24, 21,108,104,2]
+a_trier = [14, 12, 16, 8, 9, 10, 24, 21, 108, 104, 2]
 
-
-def triage():
+def tri_1():
+    start_time = time.time()
     trier = []
     while len(a_trier) > 0:
         
         trier_max = max(a_trier)
         trier.append(trier_max)
         a_trier.remove(trier_max)
+
+    end_time = time.time()
+
+    print(f"Temps exécution TRI 1 = {end_time - start_time}") 
     return trier
 
-print(triage())
+def tri_2():
+    start_time = time.time()
+    # parcourir le tableau de manière globale
+    for i in range(0, len(a_trier)):
+        # parcourir le reste du tableau pour comparaison
+        for j in range(0, len(a_trier[i+1:])):
+            # quand le nombre qui se trouve en j est + grand
+            # alors permuter avec le nombre en i
+            if a_trier[i] < a_trier[i + j]:
+               temp_value = a_trier[i]
+               a_trier[i] = a_trier[i + j]
+               a_trier[i + j] = temp_value
+
+    end_time = time.time()
+
+    print(f"Temps exécution TRI 2 = {end_time - start_time}") 
+    return a_trier
+
+#print(tri_1())
+#print(tri_2())
